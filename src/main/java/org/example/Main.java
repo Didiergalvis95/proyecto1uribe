40a27bdfee883532cbf24552a99af11fe46fcbfb
401 <401@academicos.cesde.edu.co>
2022-11-11 11:30:09 -0500

    se actualiza clase main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
         Lugar objetoLugar = new Lugar();
         Fiesta objetoFiesta = new Fiesta();
 
-        System.out.println("****** GRAN FIESTA GRUPO URIBE********");
+        /*System.out.println("****** GRAN FIESTA GRUPO URIBE********");
         System.out.println("**************************************");
 
         //PEDIR LOS DATOS DE LA FIESTA:
@@ -37,12 +37,28 @@ public class Main {
         System.out.print("Digita el aforo: ");
         objetoLugar.setAforo(entradaPorTeclado.nextInt());
         System.out.print("Digita la direccion: ");
-        objetoLugar.setDireccion(entradaPorTeclado.next());
+        objetoLugar.setDireccion(entradaPorTeclado.next());*/
 
+        System.out.println("CREANDO ARREGLOS");
+        // ARREGLOS PRIMITIVOS
+        int[] numeros = new int[5];
 
+        /*numeros[0]=10;
+        numeros[1]=20;
+        numeros[2]=30;
+        numeros[3]=40;
+        numeros[4]=50;*/
 
+        /**/
 
+        for (int i=0; i<numeros.length; i++){
+            System.out.println("ingrese un numero :");
+            numeros[i]= entradaPorTeclado.nextInt();
+        }
 
+        for (int i=0; i<numeros.length; i++){
+            System.out.println(numeros[i]);
+        }
 
 
 
