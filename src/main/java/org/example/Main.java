57d365c56689a52ce3e465096429605344db6223
Didiergalvis95 <didiergalvis95@hotmail.com>
2022-11-17 12:12:54 -0500

    actualizacion del programa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,52 +15,91 @@ public class Main {
         Lugar objetoLugar = new Lugar();
         Fiesta objetoFiesta = new Fiesta();
 
-        /*System.out.println("****** GRAN FIESTA GRUPO URIBE********");
+        System.out.println("****** GRAN FIESTA GRUPO URIBE********");
         System.out.println("**************************************");
 
-        //PEDIR LOS DATOS DE LA FIESTA:
-        System.out.println("Datos de la fiesta: ");
-        System.out.println("**************************************\n");
-        System.out.print("Fecha: ");
-        objetoFiesta.setFecha(entradaPorTeclado.nextLine());
-        System.out.print("Costos de alimentos: ");
-        objetoFiesta.setCostosAlimentos(entradaPorTeclado.nextDouble());
-        System.out.print("Costos de bebidas: ");
-        objetoFiesta.setCostosBebidas(entradaPorTeclado.nextDouble());
-        System.out.print("Costos de lugar: ");
-        objetoFiesta.setCostosLugar(entradaPorTeclado.nextDouble());
-        System.out.print("Costos de equipos: ");
-        objetoFiesta.setCostosEquipos(entradaPorTeclado.nextDouble());
-
-        System.out.println("\nDatos del Lugar: ");
-        System.out.println("**********************");
-        System.out.print("Digita el aforo: ");
+        System.out.println("Ingrese el aforo del lugar de la fiesta: ");
         objetoLugar.setAforo(entradaPorTeclado.nextInt());
-        System.out.print("Digita la direccion: ");
-        objetoLugar.setDireccion(entradaPorTeclado.next());*/
 
-        System.out.println("CREANDO ARREGLOS");
-        // ARREGLOS PRIMITIVOS
-        int[] numeros = new int[5];
-
-        /*numeros[0]=10;
-        numeros[1]=20;
-        numeros[2]=30;
-        numeros[3]=40;
-        numeros[4]=50;*/
-
-        /**/
-
-        for (int i=0; i<numeros.length; i++){
-            System.out.println("ingrese un numero :");
-            numeros[i]= entradaPorTeclado.nextInt();
-        }
-
-        for (int i=0; i<numeros.length; i++){
-            System.out.println(numeros[i]);
-        }
+        int aforoGlobal = objetoLugar.getAforo();
+        Invitado[] invitados = new Invitado[objetoLugar.getAforo()];
 
 
+
+
+
+        //CICLO PARA UBICAR INVITADOS DENTRO DEL AFORO
+        int controlRegistros =0;
+        System.out.println("Menu del programa");
+        System.out.println("**********************");
+        System.out.println("0. SALIR \n1. REGISTRO DEL LUGAR \n2. REGISTRO DE LA FIESTA \n3. REGISTRO INVITADO \n4. VER INVITADOS");
+        do {
+            System.out.print("Digite la acccion que desea realizar: ");
+            controlRegistros=entradaPorTeclado.nextInt();
+
+            switch (controlRegistros){
+                case 1:
+                    System.out.print("Digita el aforo: ");
+                    objetoLugar.setAforo(entradaPorTeclado.nextInt());
+                    System.out.print("Digita la direccion: ");
+                    objetoLugar.setDireccion(entradaPorTeclado.next());
+                    break;
+
+                case 2:
+                    System.out.print("Fecha: ");
+                    objetoFiesta.setFecha(entradaPorTeclado.next());
+                    System.out.print("Costos de alimentos: ");
+                    objetoFiesta.setCostosAlimentos(entradaPorTeclado.nextDouble());
+                    System.out.print("Costos de bebidas: ");
+                    objetoFiesta.setCostosBebidas(entradaPorTeclado.nextDouble());
+                    System.out.print("Costos de lugar: ");
+                    objetoFiesta.setCostosLugar(entradaPorTeclado.nextDouble());
+                    System.out.print("Costos de equipos: ");
+                    objetoFiesta.setCostosEquipos(entradaPorTeclado.nextDouble());
+                    break;
+
+                case 3:
+
+                    System.out.println("El aforo va en: " + objetoLugar.getAforo());
+
+
+                    int contadorInvitado = aforoGlobal -objetoLugar.getAforo();
+
+
+                    System.out.print("Ingrese el nombre del invitado: ");
+                    invitados[contadorInvitado] = new Invitado();
+                    invitados[contadorInvitado].setNombres((entradaPorTeclado.next()));
+
+                    objetoLugar.setAforo(objetoLugar.getAforo()-1);
+
+                    /*System.out.print("ingrese el documento: ");
+                    objetoInvitado.setId(entradaPorTeclado.next());
+                    System.out.print("Ingrese la edad: ");
+                    objetoInvitado.setEdad(entradaPorTeclado.nextInt());
+                    System.out.print("Este invitado paga la enrada?");
+                    objetoInvitado.setAplicaCover(entradaPorTeclado.nextBoolean());*/
+
+
+
+
+                    break;
+
+                case 4:
+                    for (int i=0; i<invitados.length; i++){
+                        System.out.println(invitados[i].getNombres());
+
+                    }
+                    break;
+
+                default:
+                    System.out.println("SELECCIONA UNA OPCION VALIDA!!!!!!");
+                    break;
+            }
+
+
+
+
+        }while (controlRegistros != 0);
 
 
 
